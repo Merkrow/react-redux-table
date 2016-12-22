7cc60d978947303b4837fb79866200fe090358cd
Sergey Pliev <merkrow@gmail.com>
2016-12-22 17:32:58 +0200

    no message

--- src/components/Students.js
+++ src/components/Students.js
@@ -10,7 +10,7 @@ const Students = React.createClass({
 					{ this.props.addingStudent && typeof this.props.addingStudent === "boolean" && <AddItem addStudent={(a,b,c) => this.props.actions.addStudent(a,b,c)} hideAdd={() => this.props.actions.hideAddStudent()} />}
 					{ this.props.addingStudent && typeof this.props.addingStudent === "number" && <AddItem changeId={this.props.addingStudent} remove={id => this.props.actions.remove(id)} item={this.props.students.students[this.props.addingStudent-1]} changeStudent={(a,b,c,d) => this.props.actions.changeStudent(a,b,c,d)} hideAdd={() => this.props.actions.hideAddStudent()} />}
 					<div>
-						{ this.props.students.students.map((item) => {
+						{ !this.props.addingStudent && this.props.students.students.map((item) => {
 								return <Item key={item.id} item={item}
 								toggleChange={id => this.props.actions.toggleChange(id)}
 								remove={(id) => this.props.actions.remove(id)} />
